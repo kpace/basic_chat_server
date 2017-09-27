f376e8595abc00bfe3be3aeeba8db987561efdf8
kpace <krasimirva@gmail.com>
2017-09-27 09:24:56 +0300

    Add username functionality

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const bodyParser = require('body-parser');
 
 app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+let people = {};
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/enter', (req, res) => {
+  people[req.body.id] = req.body.name;
+  res.status(200).send();
 });
 
 io.on('connection', (socket) => {
   console.log(`A user with id ${socket.id} connected`);
 
   socket.on('message sent', (message) => {
-    io.sockets.emit('message sent', message);
+    io.sockets.emit('message sent', {
+      message,
+      user: people[socket.id]
+    });
   });
 
   socket.on('disconnect', () => {
     console.log(`A user with id ${socket.id} disconnected`);
+
+    delete people[socket.id];
   });
 });
 
--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,29 @@ $(function(){
   var $textInput = $('#text-input');
   var $sendButton = $('#send-button');
 
+  var $nameInput = $('#name-input');
+  var $enterButton = $('#enter-button');
+
+  $enterButton.click(function() {
+    var inputValue = $nameInput.val();
+
+    if (inputValue) {
+      $.ajax({
+        url: 'enter',
+        type: "POST",
+        data: JSON.stringify({
+          id: socket.id,
+          name: inputValue
+        }),
+        contentType: 'application/json'
+      })
+      .done(function() {
+        $('#name-container').hide();
+        $('#chat-container').show();
+      });
+    }
+  });
+
   $sendButton.click(function() {
     var inputValue = $textInput.val();
     if (inputValue) {
@@ -11,8 +34,8 @@ $(function(){
     }
   });
 
-  socket.on('message sent', function(message) {
-    var p = $('<p>').text(message);
+  socket.on('message sent', function(data) {
+    var p = $('<p>').text(data.user + ' said: ' + data.message);
     $('#message-container').append(p);
   });
 });
