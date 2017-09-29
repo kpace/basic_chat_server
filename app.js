e89a3d792b831cd1c517397997d70dec54fab295
Krasimir Atanasov <katanasov@asteasolutions.com>
2017-09-29 09:06:11 +0300

    Remove unused body-parser

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-const bodyParser = require('body-parser');
-
 app.use('/public', express.static(__dirname + '/public'));
-app.use(bodyParser.json());
 
 let users = {};
 
